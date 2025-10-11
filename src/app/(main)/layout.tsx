894ede75ae95a32d8ebe9e111117bf692fedff9e
Anmol Chandrakar <crimsomemoon7@gmail.com>
2025-10-11 14:25:59 +0530

    feat: add settings page with user profile form and info bar component

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import InfoBar from "@/components/infobar";
 import Sidebar from "@/components/sidebar";
 import React from "react";
 
@@ -5,9 +6,12 @@ type Props = { children: React.ReactNode };
 
 const Layout = (props: Props) => {
   return (
-      <div className="flex overflow-hidden h-screen">
-          <Sidebar/>
-      <div className="w-full">{props.children}</div>
+    <div className="flex overflow-hidden h-screen">
+      <Sidebar />
+      <div className="w-full">
+        <InfoBar />
+        {props.children}
+      </div>
     </div>
   );
 };
