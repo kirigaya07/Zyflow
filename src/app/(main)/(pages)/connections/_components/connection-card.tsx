4e65be3cd33f311cff84bb4a4c82f6afec56844d
Anmol Chandrakar <crimsomemoon7@gmail.com>
2025-10-14 13:16:56 +0530

    fix: correct destructuring of props in ConnectionCard component

--- src/app/(main)/(pages)/connections/_components/connection-card.tsx
+++ src/app/(main)/(pages)/connections/_components/connection-card.tsx
@@ -23,8 +23,8 @@ const ConnectionCard = ({
   type,
   icon,
   title,
-}: // connected,
-Props) => {
+  connected,
+}: Props) => {
   return (
     <Card className="flex w-full items-center justify-between">
       <CardHeader className="flex flex-col gap-4">
@@ -43,26 +43,26 @@ Props) => {
         </div>
       </CardHeader>
       <div className="flex flex-col items-center gap-2 p-4">
-        {/* {connected[type] ? (
+        {connected[type] ? (
           <div className="border-bg-primary rounded-lg border-2 px-3 py-2 font-bold text-white">
             Connected
           </div>
-        ) : ( */}
-        <Link
-          href={
-            title == "Discord"
-              ? process.env.NEXT_PUBLIC_DISCORD_REDIRECT!
-              : title == "Notion"
-              ? process.env.NEXT_PUBLIC_NOTION_AUTH_URL!
-              : title == "Slack"
-              ? process.env.NEXT_PUBLIC_SLACK_REDIRECT!
-              : "#"
-          }
-          className=" rounded-lg bg-primary p-2 font-bold text-primary-foreground"
-        >
-          Connect
-        </Link>
-        {/* )} */}
+        ) : (
+          <Link
+            href={
+              title == "Discord"
+                ? process.env.NEXT_PUBLIC_DISCORD_REDIRECT!
+                : title == "Notion"
+                ? process.env.NEXT_PUBLIC_NOTION_AUTH_URL!
+                : title == "Slack"
+                ? process.env.NEXT_PUBLIC_SLACK_REDIRECT!
+                : "#"
+            }
+            className=" rounded-lg bg-primary p-2 font-bold text-primary-foreground"
+          >
+            Connect
+          </Link>
+        )}
       </div>
     </Card>
   );
