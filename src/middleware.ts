12714190acf72ea15ef1f381ff138e27c706c5d0
kirigaya07 <crimsomemoon7@gmail.com>
2026-03-15 21:45:49 +0530

    Add sign-in and sign-up routes to public route matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,8 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
   "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
   "/api/clerk-webhook",
   "/api/drive-activity/notification",
   "/api/zoom-webhook",
